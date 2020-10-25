e64d6f20c634b057bd8b6d312dc80b0c6b788d78
Square Fong <squarefong17@gmail.com>
2020-10-25 22:29:01 +0800

    add get messages function to help get messages from manager

--- online_client_manager.go
+++ online_client_manager.go
@@ -1,12 +1,15 @@
 package main
 
+import "time"
+
 type client struct {
 	effective      bool
 	detail         phoneDetail
 	messages       []message
 	needAll        bool //true: 需要上传所有，当有allMessages的post则更改
-	lastActiveTime int
+	lastActiveTime int64
 	UUID           string
+	newSMS         []message //json string
 }
 
 var allClients = []client{}
@@ -23,16 +26,19 @@ func findClientByUUID(UUID string, c **client) bool {
 
 func activateClient(UUID string) {
 	var c *client
-	if !findClientByUUID(UUID, &c) {
+	if findClientByUUID(UUID, &c) {
+		c.lastActiveTime = time.Now().Unix()
+	} else {
 		*c = client{
 			effective:      true,
 			detail:         phoneDetail{},
 			messages:       []message{},
 			needAll:        true,
 			lastActiveTime: 0,
-			UUID:           UUID}
+			UUID:           UUID,
+			newSMS:         []message{}}
+		allClients = append(allClients, *c)
 	}
-	allClients = append(allClients, *c)
 }
 
 //当get的是detail/message/allMessage/,检查是否有UUID
@@ -51,6 +57,17 @@ func getMessages(UUID string) *[]message {
 	if findClientByUUID(UUID, &c) {
 		m := c.messages
 		c.messages = []message{}
+		return &m
+	}
+	return nil
+}
+
+func getNewMessages(UUID string) *[]message {
+	activateClient(UUID)
+	var c *client
+	if findClientByUUID(UUID, &c) {
+		m := c.newSMS
+		c.newSMS = []message{}
 		return &m
 	}
 	return nil
